49e18c66b1fe5b92898e9e5bef582b4ba8c02948
Jan Weidner <jw3126@gmail.com>
2021-03-24 23:49:05 +0100

    init

--- src/LinearInterpolations.jl
+++ src/LinearInterpolations.jl
@@ -1,5 +1,191 @@
 module LinearInterpolations
 
-# Write your package code here.
+export neighbors_and_weights, linterpol, Linterpol
+
+using ArgCheck
+
+struct MapProductArray{T,N,F,Factors} <: AbstractArray{T,N}
+    f::F
+    factors::Factors
+end
+
+function MapProductArray(f::F, factors::Factors) where {F, Factors}
+    N = length(factors)
+    pt = map(first, factors)
+    T = typeof(f(pt))
+    return MapProductArray{T,N,F,Factors}(f, factors)
+end
+
+function Base.getindex(o::MapProductArray{T,N}, I::Vararg{Int, N}) where {T,N}
+    @boundscheck checkbounds(o, I...)
+    xs = map(o.factors, I) do v, i
+        @inbounds v[i]
+    end
+    return o.f(xs)
+end
+
+@inline function field_type(::Type{T}) where {T}
+    T
+end
+
+@inline function field_type(::Type{S}, ::Type{T}) where {S, T}
+    t = one(T)
+    s = one(S)
+    typeof((t - s) / (t + s))
+end
+
+@inline function field_type(S, T, Ts...)
+    field_type(field_type(S, T), Ts...)
+end
+
+const ALLOWED_ONOUTSIDE_VALUES = [:replicate, :reflect, :error]
+
+
+Base.size(o::MapProductArray) = map(length, o.factors)
+
+function neighbors_and_weights1d_outside(xs, x, onoutside)
+    if onoutside === :error
+        msg = """
+            x=$x is not between first(xs)=$(first(xs)) and last(xs)=$(last(xs))
+            You can suppress this error by passing the `onoutside` argument.
+            """
+            throw(ArgumentError(msg))
+    elseif onoutside === :reflect
+        x_first = first(xs)
+        x_last = last(xs)
+        x_new = if x > x_last
+            2x_last - x
+        elseif x < x_first
+            2x_first - x
+        else
+            msg = "Cannot apply onoutside=$onoutside to x=$x"
+            throw(ArgumentError(msg))
+        end
+        return neighbors_and_weights1d(xs, x_new, onoutside)
+    elseif onoutside === :replicate
+        x_inside = clamp(x, first(xs), last(xs))
+        return neighbors_and_weights1d(xs, x_inside, :error)
+    else
+        msg = """Unknown onoutside = $onoutside
+        Allowed values are:
+        $ALLOWED_ONOUTSIDE_VALUES
+        """
+        throw(ArgumentError(msg))
+    end
+end
+
+struct TinyVector{T} <: AbstractVector{T}
+    elements::NTuple{2,T}
+    is_length_two::Bool
+end
+
+TinyVector(x)   = TinyVector((x,x), false)
+TinyVector(x,y) = TinyVector((x,y), true)
+
+function Base.size(o::TinyVector)
+    (ifelse(o.is_length_two, 2, 1),)
+end
+
+function Base.getindex(o::TinyVector, i::Integer)
+    @boundscheck checkbounds(o, i)
+    @inbounds ifelse(i == 1, o.elements[1], o.elements[2])
+end
+
+function neighbors_and_weights1d(xs, x, onoutside=:error)
+    @argcheck !isempty(xs)
+    x_inside = clamp(x, first(xs), last(xs))
+    is_outside = !(x ≈ x_inside)
+    if is_outside
+        return neighbors_and_weights1d_outside(xs, x, onoutside)
+    end
+    x = x_inside
+    ixu = searchsortedfirst(xs,x)
+    xu = xs[ixu]
+    if x ≈ xu
+        T   = field_type(typeof(x), eltype(xs))
+        wts  = TinyVector(one(T))
+        nbs = ixu:ixu
+    else
+        ixl = ixu - 1
+        xl = xs[ixl]
+        wl = (xu - x)/(xu - xl)
+        wu = (x - xl)/(xu - xl)
+        wts = TinyVector(wl, wu)
+        nbs = ixl:ixu
+    end
+    return (nbs, wts)
+end
+
+function neighbors_and_weights(axes::NTuple{N, Any}, pt; onoutside=:error) where {N}
+    @argcheck length(axes) == length(pt)
+    nbs_wts = map(axes, NTuple{N}(pt)) do xs, x
+        neighbors_and_weights1d(xs, x, onoutside)
+    end
+    wts = MapProductArray(prod, map(last, nbs_wts))
+    nbs = map(first, nbs_wts)
+    return nbs, wts
+end
+
+function combine(wts, objs)
+    mapreduce(*, +, wts, objs)
+end
+
+function linterpol(axes, objs, pt; onoutside=:error, combine::C=combine) where {C}
+    itp = Linterpol(combine, axes, objs, onoutside)
+    return itp(pt)
+end
+
+function linterpol(xs::AbstractVector, ys::AbstractVector, pt; kw...)
+    axes = (xs,)
+    return linterpol(axes, ys, pt; kw...)
+end
+
+struct Linterpol{C,A,V,O}
+    combine::C
+    axes::A
+    values::V
+    onoutside::O
+    function Linterpol(combine, axes, values, onoutside)
+        @argcheck size(values) == map(length, axes)
+        @argcheck onoutside in ALLOWED_ONOUTSIDE_VALUES
+        # some sanity checks, all of them O(ndims(values))
+        # we assume issorted(xs), which wouldbe O(length(values)) to check
+        @argcheck all( (!isempty).(axes)                      )
+        @argcheck all( isfinite.(first.(axes))                )
+        @argcheck all( isfinite.(last.(axes))                 )
+        @argcheck all( first.(axes) .<= last.(axes)           )
+        @argcheck all( ndims.(axes) .== 1                     )
+        @argcheck all( eachindex.(axes) .== Base.axes(values) )
+        C = typeof(combine)
+        A = typeof(axes)
+        V = typeof(values)
+        O = typeof(onoutside)
+        return new{C,A,V,O}(combine, axes, values, onoutside)
+    end
+end
+
+function Linterpol(xs::AbstractVector, ys::AbstractVector; combine=combine, onoutside=:error)
+    return Linterpol(combine, (xs,), ys, onoutside)
+end
+function Linterpol(axes, values; combine=combine, onoutside=:error)
+    return Linterpol(combine, axes, values, onoutside)
+end
+
+axestype(::Linterpol{C,A}) where {C,A} = A
+axestype(::Type{<:Linterpol{C,A}}) where {C,A} = A
+_length(::Type{<:NTuple{N,Any}}) where {N} = N
+Base.ndims(L::Type{<:Linterpol}) = _length(axestype(L))
+Base.ndims(o::Linterpol) = ndims(typeof(o))
+
+function (o::Linterpol)(pt)
+    @argcheck length(pt) == ndims(o)
+    pt2 = NTuple{ndims(o)}(pt)
+    return o(pt2)
+end
+
+function (o::Linterpol)(pt::Tuple)
+    nbs, wts = neighbors_and_weights(o.axes, pt; onoutside=o.onoutside)
+    o.combine(wts, view(o.values, nbs...))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,123 @@
 using LinearInterpolations
+using LinearInterpolations: neighbors_and_weights1d
 using Test
+using ArgCheck
 
-@testset "LinearInterpolations.jl" begin
-    # Write your tests here.
+@testset "neighbors_and_weights" begin
+    @inferred neighbors_and_weights((1:3,), 2)
+    @inferred neighbors_and_weights(([1., 2.],), Float32(2))
+    @inferred neighbors_and_weights(([1., 2.],Base.OneTo(10)), [1,2])
+
+    xs = 1:10
+    x = 3.3
+    axs = (xs,)
+    nbs, wts = @inferred neighbors_and_weights(axs, x)
+    @test nbs == (3:4,)
+    @test wts ≈ [0.7, 0.3]
+
+    axs = (1:10, 2:3)
+    pt = (3.3, 2.0)
+    nbs, wts = @inferred neighbors_and_weights(axs, pt)
+    @test nbs == (3:4, 1:1)
+    @test wts ≈ [0.7, 0.3]
+
+end
+
+@testset "neighbors_and_weights1d" begin
+    @inferred neighbors_and_weights1d([10, 20], 30, :replicate)
+
+    @test neighbors_and_weights1d([10, 20, 30], 10) == (1:1, [1.0])
+    @test neighbors_and_weights1d([10, 20, 30], 11) == (1:2, [0.9, 0.1])
+    @test neighbors_and_weights1d([10, 20, 30], 11) == (1:2, [0.9, 0.1])
+    @test neighbors_and_weights1d([10, 20, 30], 20) == (2:2, [1.0])
+    @test neighbors_and_weights1d([10, 20, 30], 25) == (2:3, [0.5, 0.5])
+    @test neighbors_and_weights1d([10, 20, 30], 30) == (3:3, [1.0])
+    @test neighbors_and_weights1d([10, 20], 30, :replicate) == (2:2, [1.0])
+    @test neighbors_and_weights1d([10, 20], 0, :replicate) == (1:1, [1.0])
+    @test neighbors_and_weights1d([10, 20], 25, :reflect) == (1:2, [0.5, 0.5])
+    @test neighbors_and_weights1d([10, 20], 5, :reflect) == (1:2, [0.5, 0.5])
+    @test neighbors_and_weights1d([10, 20], 35, :reflect) == (1:2, [0.5, 0.5])
+
+    @test_throws ArgumentError neighbors_and_weights1d([10, 20], 30, :error)
+    @test_throws ArgumentError neighbors_and_weights1d([10, 20], 30, :nonsense)
+    @test_throws ArgumentError neighbors_and_weights1d([10, 20, 30], 9)
+    @test_throws ArgumentError neighbors_and_weights1d([10, 20, 30], 31)
+end
+
+@testset "1d linterpol" begin
+    for _ in 1:100
+        xs = sort!(randn(10)); ys = randn(10);
+        onoutside = rand(LinearInterpolations.ALLOWED_ONOUTSIDE_VALUES)
+        if onoutside == :error
+            x = clamp(randn(), xs[1], xs[end])
+        else
+            x = randn()
+        end
+        @test linterpol(xs, ys, x; onoutside=onoutside) ===
+            Linterpol(xs, ys; onoutside=onoutside)(x)
+    end
+
+    @test linterpol(1:2, [10, 20], 1) ≈ 10
+    @test Linterpol(1:2, [10, 20])(1) ≈ 10
+    @test linterpol(1:2, [10, 20], 1.5) ≈ 15
+    @test linterpol(1:2, [10, 20], 2) ≈ 20
+    @test linterpol(1:2, [10, 20], 2f0) === 20f0
+    @test linterpol(1:2, [10, 20], 2.0) === 20.0
+    for onoutside in [:error, :reflect, :replicate]
+        @test linterpol(1:2, [10, 20], 2.0; onoutside) === 20.0
+    end
+    @test linterpol([10, 20], [2, 1], 30000, onoutside=:replicate) ≈ 1
+    @test linterpol([10, 20], [2, 1], 30, onoutside=:reflect) ≈ 2
+    @test linterpol([10, 20], [2, 1], 30, onoutside=:reflect) ≈ 2
+    @test linterpol([10, 20], [2, 1], 22.5, onoutside=:reflect) ≈ 1.25
+
+    @inferred linterpol(1:2, [10, 20], 1)
+    @inferred linterpol(1:2, [10, 20], 1f0)
+    @inferred linterpol(1:2, [10, 20], 1.0)
+    @inferred linterpol(1:2, [10, 20], 1.0, onoutside=:error)
+    @test_throws ArgumentError linterpol(1:2, [10, 20, 30], 0.9)
+    @test_throws ArgumentError linterpol(1:2, [10, 20, 30], 2.1)
+    @test_throws ArgumentError linterpol(1:2, [10, 20], 2.1, onoutside=:nonsense)
+    @test_throws ArgumentError Linterpol(1:2, [10, 20], onoutside=:nonsense)
+
+    @testset "double points" begin
+        @test linterpol([1,1,2], [10,20,30], 1.5) ≈ 25
+        @test linterpol([1,1,2], [10,20,30], 1.000000001) ≈ 20
+        @test 10 <= linterpol([1,1,2], [10,20,30], 1.0) <= 20
+
+        @test linterpol([0,1,1,2], [0,10,20,30], 1.5) ≈ 25
+        @test linterpol([0,1,1,2], [0,10,20,30], 1.000000001) ≈ 20
+        @test 10 <= linterpol([0,1,1,2], [0,10,20,30], 1.0) <= 20
+    end
+end
+
+@testset "Probability distributions" begin
+    struct ProbDist
+        probabilities::Vector{Float64}
+        function ProbDist(ps)
+            @argcheck sum(ps) ≈ 1
+            @argcheck all(p -> p>=0, ps)
+            new(Vector{Float64}(ps))
+        end
+    end
+
+    function LinearInterpolations.combine(wts, dists::AbstractArray{ProbDist})
+        ps = LinearInterpolations.combine(wts, map(dist-> dist.probabilities, dists))
+        ProbDist(ps)
+    end
+
+    wts  = [0.1, 0.9]
+    dists = [ProbDist([0.5, 0.5]), ProbDist([1, 0])]
+    dist = LinearInterpolations.combine(wts, dists)
+    @test dist isa ProbDist
+    @test dist.probabilities ≈ [0.95, 0.05]
+
+    xs = [10, 20]
+    pt = 19
+    @test linterpol(xs, dists, pt).probabilities ≈ [0.95, 0.05]
+    @test Linterpol(xs, dists)(pt).probabilities ≈ [0.95, 0.05]
+
+    @inferred linterpol(xs, dists, pt)
+    @inferred Linterpol(xs, dists)(pt)
+
 end
